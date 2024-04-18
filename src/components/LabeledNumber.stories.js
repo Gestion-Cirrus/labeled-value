ad65bbe244885bc3d082b619a44b66b3d053aa87
Frank <mtlqrf@gmail.com>
2024-04-18 09:08:18 -0400

    aider: Created Storybook stories for `LabeledString` and `LabeledNumber` components.
    # Aider chat conversation:
    
    USER: continue writing stories for all components
    
    ASSISTANT: Continuing with the creation of Storybook stories for the remaining components, I will start with the `LabeledString` component.
    
    ```diff
    --- /dev/null
    +++ src/components/LabeledString.stories.js
    @@ ... @@
    +import React from 'react';
    +import LabeledString from './LabeledString';
    +
    +export default {
    +  title: 'LabeledString',
    +  component: LabeledString,
    +};
    +
    +export const Default = () => (
    +  <LabeledString label="String" value="This is a string" />
    +);
    +
    +export const EmptyString = () => (
    +  <LabeledString label="String" value="" />
    +);
    +
    +export const InvalidType = () => (
    +  <LabeledString label="String" value={1234} />
    +);
    ```
    
    Next, the `LabeledNumber` component:
    
    ```diff
    --- /dev/null
    +++ src/components/LabeledNumber.stories.js
    @@ ... @@
    +import React from 'react';
    +import LabeledNumber from './LabeledNumber';
    +
    +export default {
    +  title: 'LabeledNumber',
    +  component: LabeledNumber,
    +};
    +
    +export const Default = () => (
    +  <LabeledNumber label="Number" value={42} />
    +);
    +
    +export const WithUnit = () => (
    +  <LabeledNumber label="Weight" value={70} unit="kg" />
    +);
    +
    +export const InvalidType = () => (
    +  <LabeledNumber label="Number" value="Not a number" />
    +);
    ```
    
    I will continue to create story files for each of the remaining components. If you would like to see the diffs for a specific component or a set of components, please let me know.

---
+++ src/components/LabeledNumber.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import LabeledNumber from './LabeledNumber';
+
+export default {
+  title: 'LabeledNumber',
+  component: LabeledNumber,
+};
+
+export const Default = () => (
+  <LabeledNumber label="Number" value={42} />
+);
+
+export const WithUnit = () => (
+  <LabeledNumber label="Weight" value={70} unit="kg" />
+);
+
+export const InvalidType = () => (
+  <LabeledNumber label="Number" value="Not a number" />
+);
---
+++ src/components/LabeledString.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import LabeledString from './LabeledString';
+
+export default {
+  title: 'LabeledString',
+  component: LabeledString,
+};
+
+export const Default = () => (
+  <LabeledString label="String" value="This is a string" />
+);
+
+export const EmptyString = () => (
+  <LabeledString label="String" value="" />
+);
+
+export const InvalidType = () => (
+  <LabeledString label="String" value={1234} />
+);
