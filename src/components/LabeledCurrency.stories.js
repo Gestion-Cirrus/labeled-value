87e023d100aab134051d36516a826ef163682381
Frank <mtlqrf@gmail.com>
2024-04-18 14:56:30 -0400

    aider: Added LabeledCurrency stories without prepending 'components/' to the title.
    # Aider chat conversation:
    
    USER: do one for LabeledCurrency, please dont prepend 'components/' to the title.
    
    ASSISTANT: ```diff
    --- /dev/null
    +++ src/components/LabeledCurrency.stories.js
    @@ ... @@
    +import React from 'react';
    +import LabeledCurrency from './LabeledCurrency';
    +
    +export default {
    +  title: 'LabeledCurrency',
    +  component: LabeledCurrency,
    +};
    +
    +const Template = (args) => <LabeledCurrency {...args} />;
    +
    +export const Default = Template.bind({});
    +Default.args = {
    +  label: 'Total Balance',
    +  value: '1024.50',
    +  currency: 'USD',
    +};
    +
    +export const WithLargeAmount = Template.bind({});
    +WithLargeAmount.args = {
    +  label: 'Debt',
    +  value: '987654321.99',
    +  currency: 'EUR',
    +};
    ```

---
+++ src/components/LabeledCurrency.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import LabeledCurrency from './LabeledCurrency';
+
+export default {
+  title: 'LabeledCurrency',
+  component: LabeledCurrency,
+};
+
+const Template = (args) => <LabeledCurrency {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Total Balance',
+  value: '1024.50',
+  currency: 'USD',
+};
+
+export const WithLargeAmount = Template.bind({});
+WithLargeAmount.args = {
+  label: 'Debt',
+  value: '987654321.99',
+  currency: 'EUR',
+};
