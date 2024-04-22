4b70e2f0c9f24e2b0361b0e3480d7c54cca696f4
Frank <mtlqrf@gmail.com>
2024-04-22 15:33:04 -0400

    trying to build react compatible

--- src/index.js
+++ src/index.js
@@ -8,18 +8,26 @@
  * the root directory of this source tree or available online at [https://opensource.org/license/isc-license-txt].
  */
 
-export { default as LabeledVoid } from './components/LabeledVoid';
-export { default as LabeledString } from './components/LabeledString';
-export { default as LabeledNumber } from './components/LabeledNumber';
-export { default as LabeledDate } from './components/LabeledDate';
-export { default as LabeledBoolean } from './components/LabeledBoolean';
-export { default as LabeledJson } from './components/LabeledJson';
-export { default as LabeledGeoJson } from './components/LabeledGeoJson';
-export { default as LabeledEmail } from './components/LabeledEmail';
-export { default as LabeledImage } from './components/LabeledImage';
-export { default as LabeledVideo } from './components/LabeledVideo';
-export { default as LabeledAudio } from './components/LabeledAudio';
-export { default as LabeledFile } from './components/LabeledFile';
-export { default as LabeledColor } from './components/LabeledColor';
-export { default as LabeledTree } from './components/LabeledTree';
-export { default as LabeledRaw } from './components/LabeledRaw';
+export {default as LabeledAudio} from './components/LabeledAudio';
+export {default as LabeledBoolean} from './components/LabeledBoolean';
+export {default as LabeledColor} from './components/LabeledColor';
+export {default as LabeledCurrency} from './components/LabeledCurrency';
+export {default as LabeledDate} from './components/LabeledDate';
+export {default as LabeledEmail} from './components/LabeledEmail';
+export {default as LabeledFile} from './components/LabeledFile';
+export {default as LabeledGeoJson} from './components/LabeledGeoJson';
+export {default as LabeledImage} from './components/LabeledImage';
+export {default as LabeledJson} from './components/LabeledJson';
+export {default as LabeledLink} from './components/LabeledLink';
+export {default as LabeledNumber} from './components/LabeledNumber';
+export {default as LabeledPercentage} from './components/LabeledPercentage';
+export {default as LabeledRating} from './components/LabeledRating';
+export {default as LabeledRaw} from './components/LabeledRaw';
+export {default as LabeledString} from './components/LabeledString';
+export {default as LabeledTimeline} from './components/LabeledTimeline';
+export {default as LabeledTree} from './components/LabeledTree';
+export {default as LabeledValue} from './components/LabeledValue';
+export {default as LabeledVideo} from './components/LabeledVideo';
+export {default as LabeledVoid} from './components/LabeledVoid';
+
+
